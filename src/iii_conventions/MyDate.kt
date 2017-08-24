c31b4ce89bb34b393528baa110d108e3ac457ae4
tpb1908 <theo1908phone@gmail.com>
2017-08-24 12:08:26 +0100

    Implement conventions stuff.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,39 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    override fun compareTo(other: MyDate): Int = when {
+        year > other.year -> 1
+        year < other.year -> -1
+        month > other.month -> 1
+        month < other.month -> -1
+        dayOfMonth > other.dayOfMonth -> 1
+        dayOfMonth < other.dayOfMonth -> -1
+        else -> 0
+    }
+
+    /*
+Cleaner
+when {
+    year != other.year -> year - other.year
+    month != other.month -> month - other.month
+    else -> dayOfMonth - other.dayOfMonth
+}
+
+ */
+
+    operator fun plus(interval: TimeInterval): MyDate {
+        return this.addTimeIntervals(interval, 1)
+    }
+
+    operator fun plus(interval: RepeatedTimeInterval): MyDate {
+        return this.addTimeIntervals(interval.interval, interval.number)
+    }
+
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +41,24 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class RepeatedTimeInterval(val interval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
+
+    operator fun contains(date: MyDate): Boolean = start <= date && date <= endInclusive
+
+    override fun iterator(): Iterator<MyDate> = object: Iterator<MyDate> {
+        var current = this@DateRange.start
+
+        override fun hasNext(): Boolean = current <= this@DateRange.endInclusive
+
+        override fun next(): MyDate {
+            val result = current
+            current = current.addTimeIntervals(TimeInterval.DAY, 1)
+            return result
+        }
+    }
+}
